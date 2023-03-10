14fbaaa45f2a1d8f5ea2434a3c9d66e79ab96dd2
mayyar <mayyar2001@hotmail.com>
2023-03-10 19:06:13 +0300

    Added custom user profile model , manager and migrations.01

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -16,7 +16,7 @@ class UserProfileManager(BaseUserManager):
         user=self.model(email=email,name=name)
 
         user.set_password(password)
-        user.save(using=self._dp)
+        user.save(using=self._db)
 
         return user
 
@@ -26,7 +26,7 @@ class UserProfileManager(BaseUserManager):
         user = self.create_user(email,name,password)
         user.is_superuser=True
         user.is_staff=True
-        user.save(using=self._dp)
+        user.save(using=self._db)
 
 
 
@@ -44,7 +44,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     objects=UserProfileManager()
 
     USERNAME_FIELD='email'
-    REQUIRED_FIELDs=['name']
+    REQUIRED_FIELDS=['name']
 
     def get_full_name(self):
         """ Retrieve full name of user """
